b8bd84870ba648ed26a86f6fe499fbca5c61cfcb
2559055 <mohamed.hussein@student.moringa.school.com>
2023-10-06 06:32:53 -0700

    my control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
-}
+  if (rideDistance <= 400){
+    return "gives customers a free sample";
+  
+  }
+  else if (rideDistance > 2000)
+  {
+return "charges 30 dollars for a distance over 2000 feet";
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
+  else if (rideDistance > 2500)
+  {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+    return "does not allow rides over 2500 feet";
+  }
+  
 }
+console.log(scuberGreetingForFeet(400))
+
+
+function ternaryCheckCity(nycDistance){
+    // Write your code here!
+    if (nycDistance){
+        return "ok";
+    }
+
+else {
+    return "no go";
+}
+}
+    
+
+console.log(ternaryCheckCity(false))
+   
+function switchOnCharmFromTip(tibIsGenerous){
+ // Write your code here!
+ if (tibIsGenerous){
+    return "thank you so much";
+ }  else
+ {return "thank you";
+} 
+;
+    
+ 
+ 
+
+}
+console.log(switchOnCharmFromTip(true))
